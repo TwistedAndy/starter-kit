00e8a6b67de12240136b47d99766b026359e8272
Toniievych Andrii <toniyevych@gmail.com>
2021-04-20 12:33:50 +0200

    Implemented small layout changes

--- template/scripts/viewport.js
+++ template/scripts/viewport.js
@@ -1,9 +1,31 @@
 function lockScroll() {
 	document.body.classList.add('is_locked');
+	document.body.style.paddingRight = getScrollbarWidth() + 'px';
 }
 
 function unlockScroll() {
 	document.body.classList.remove('is_locked');
+	document.body.style.paddingRight = '0px';
+}
+
+function getScrollbarWidth() {
+
+	var outer = document.createElement('div');
+	var inner = document.createElement('div');
+
+	outer.style.visibility = 'hidden';
+	outer.style.overflow = 'scroll';
+	outer.style.msOverflowStyle = 'scrollbar';
+	outer.appendChild(inner);
+
+	document.body.appendChild(outer);
+
+	var scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+	outer.parentNode.removeChild(outer);
+
+	return scrollbarWidth;
+
 }
 
 function fixViewportHeight() {
