e6fc5c069386d832a9df1f49bceb2a255748767b
Andrii Toniievych <toniyevych@gmail.com>
2024-01-23 10:32:05 +0100

    Changed the script initialization logic

--- template/scripts/fancybox.js
+++ template/scripts/fancybox.js
@@ -1,19 +1,13 @@
-/* Image popup */
-
-jQuery(function($) {
+jQuery(document.body).on('tw_init', '[class*="_box"]', function(e, $) {
 
 	if (typeof Fancybox !== 'function') {
 		return;
 	}
 
-	$('section').each(function() {
-
-		Fancybox.bind(this, 'a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"], a[href$=".gif"]', {
-			groupAll: true
-		});
-
-		Fancybox.bind(this, 'a[href*="youtube.com"], a[href*="youtu.be"], a[href*="vimeo.com"], a[href$="mp4"]', {});
-
+	Fancybox.bind(this, 'a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"], a[href$=".gif"]', {
+		groupAll: true
 	});
+
+	Fancybox.bind(this, 'a[href*="youtube.com"], a[href*="youtu.be"], a[href*="vimeo.com"], a[href$="mp4"]', {});
 
 });
